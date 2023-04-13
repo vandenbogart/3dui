c251aba9206a501048102ac2de01a9bb106593a7
Eric Bogard <eric.bogard@bitsighttech.com>
2023-04-13 15:17:29 -0700

    Render a triangle

--- threedui/src/main.rs
+++ threedui/src/main.rs
@@ -1,3 +1,150 @@
+use std::borrow::Cow;
+
+use winit::{event_loop::{ControlFlow, EventLoop}, window::WindowBuilder, event::{WindowEvent, Event}};
+
+async fn run(event_loop: EventLoop<()>, window: winit::window::Window) {
+    let size = window.inner_size();
+    let instance = wgpu::Instance::default();
+    let surface = unsafe { instance.create_surface(&window) }.unwrap();
+    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::default(),
+        force_fallback_adapter: false,
+        compatible_surface: Some(&surface),
+    })
+    .await
+    .expect("Failed to find adapter");
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::downlevel_webgl2_defaults()
+                    .using_resolution(adapter.limits()),
+            },
+            None,
+            )
+        .await
+        .expect("Failed to create device and queue");
+
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+    });
+
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let swapchain_capabilities = surface.get_capabilities(&adapter);
+    let swapchain_format = swapchain_capabilities.formats[0];
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(swapchain_format.into())],
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    });
+
+    let mut surface_config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: swapchain_capabilities.alpha_modes[0],
+        view_formats: vec![],
+    };
+
+    surface.configure(&device, &surface_config);
+
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                },
+                WindowEvent::Resized(size) => {
+                    surface_config.width = size.width;
+                    surface_config.height = size.height;
+                    surface.configure(&device, &surface_config);
+                    window.request_redraw();
+                },
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                let frame = surface.get_current_texture()
+                    .expect("Failed to acquire next texture in swapchain");
+                let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
+                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: None });
+                {
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
+                                store: true,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                    });
+                    rpass.set_pipeline(&render_pipeline);
+                    rpass.draw(0..3, 0..1);
+                }
+                queue.submit(Some(encoder.finish()));
+                frame.present();
+            },
+            _ => (),
+        }
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("ThreeDUI Demo")
+        .with_inner_size(winit::dpi::LogicalSize::new(800,600))
+        .build(&event_loop)
+        .unwrap();
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let async_run = run(event_loop, window);
+        pollster::block_on(async_run);
+    }
+    // #[cfg(target_arch = "wasm32")]
+    // {
+    //     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    //     console_log::init().expect("could not initialize logger");
+    //     use winit::platform::web::WindowExtWebSys;
+    //     // On wasm, append the canvas to the document body
+    //     web_sys::window()
+    //         .and_then(|win| win.document())
+    //         .and_then(|doc| doc.body())
+    //         .and_then(|body| {
+    //             body.append_child(&web_sys::Element::from(window.canvas()))
+    //                 .ok()
+    //         })
+    //         .expect("couldn't append canvas to document body");
+    //     wasm_bindgen_futures::spawn_local(run(event_loop, window));
+    // }
 }
